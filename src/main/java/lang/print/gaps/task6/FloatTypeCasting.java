415d5daff43fe9274059c1482fbaccf8c32e2f6c
BekoSm <b.smagulov.1999@gmail.com>
2022-07-13 11:50:08 +0600

    Push#3

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,6 +2,6 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
-        int integer = Math.round(numberToBeRounded);
+        numberToBeRounded = Math.round(numberToBeRounded);
     }
 }
